77a978799cf790dcfd44d47a66d4808be55f39e0
Nikolia558 <38259407+Nikolai558@users.noreply.github.com>
2022-04-11 22:23:28 -0600

    Added Staff Command Template
    No Staff Commands Added in yet.

---
+++ FEBuddyDiscordBot/Modules/Staff/StaffCommands.cs
@@ -0,0 +1,32 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace FEBuddyDiscordBot.Modules.Staff;
+
+[Name("Staff Commands")]
+[Summary("These commands are to assist the Staff of the server.")]
+[RequireUserPermission(Discord.GuildPermission.Administrator)]
+public class StaffCommands : ModuleBase
+{
+    private readonly IServiceProvider _services;
+    private readonly IConfigurationRoot _config;
+    private readonly DiscordSocketClient _discord;
+    private readonly ILogger _logger;
+    private readonly string _prefix;
+
+    public StaffCommands(IServiceProvider services)
+    {
+        _services = services;
+        _config = _services.GetRequiredService<IConfigurationRoot>();
+        _discord = _services.GetRequiredService<DiscordSocketClient>();
+        _logger = _services.GetRequiredService<ILogger<StaffCommands>>();
+        _prefix = _config["prefix"];
+
+        _logger.LogInformation("Module: Loaded StaffCommands");
+    }
+
+    // Discord Server Staff commands go here.
+}
