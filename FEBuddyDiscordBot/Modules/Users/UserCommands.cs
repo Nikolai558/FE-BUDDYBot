bd49bc38178f1212807eab89b36a3eafaf9808e0
Nikolia558 <38259407+Nikolai558@users.noreply.github.com>
2022-04-11 22:27:42 -0600

    Added User Command Template
    No User Commands have been added yet.

---
+++ FEBuddyDiscordBot/Modules/Users/UserCommands.cs
@@ -0,0 +1,32 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace FEBuddyDiscordBot.Modules.Users;
+
+[Name("User Commands")]
+[Summary("Helpful commands for All users in the Discord server!")]
+public class UserCommands : ModuleBase
+{
+    private readonly IServiceProvider _services;
+    private readonly IConfigurationRoot _config;
+    private readonly DiscordSocketClient _discord;
+    private readonly ILogger _logger;
+    private readonly string _prefix;
+
+    public UserCommands(IServiceProvider services)
+    {
+        _services = services;
+        _config = _services.GetRequiredService<IConfigurationRoot>();
+        _discord = _services.GetRequiredService<DiscordSocketClient>();
+        _logger = _services.GetRequiredService<ILogger<UserCommands>>();
+        _prefix = _config["prefix"];
+
+        _logger.LogInformation("Module: Loaded UserCommands");
+    }
+
+    // Discord Server User commands go here.
+}
