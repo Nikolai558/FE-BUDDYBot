c1e70fd2dd3b78b625ae2fd33572152f44cf10ce
Nikolia558 <38259407+Nikolai558@users.noreply.github.com>
2022-04-11 22:21:00 -0600

    Added Owner Only Commands Template
    No owner Only commands have been added in yet.

---
+++ FEBuddyDiscordBot/Modules/Owner/OwnerCommands.cs
@@ -0,0 +1,37 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FEBuddyDiscordBot.Modules.Owner;
+
+[Name("Bot Owner Commands")]
+[Summary("These commands only effect the hardware of the computer that the bot is running on. Therfore only the bot Owner can run these commands.")]
+[RequireOwner]
+public class OwnerCommands : ModuleBase
+{
+    private readonly IServiceProvider _services;
+    private readonly IConfigurationRoot _config;
+    private readonly DiscordSocketClient _discord;
+    private readonly ILogger _logger;
+    private readonly string _prefix;
+
+    public OwnerCommands(IServiceProvider services)
+    {
+        _services = services;
+        _config = _services.GetRequiredService<IConfigurationRoot>();
+        _discord = _services.GetRequiredService<DiscordSocketClient>();
+        _logger = _services.GetRequiredService<ILogger<OwnerCommands>>();
+        _prefix = _config["prefix"];
+
+        _logger.LogInformation("Module: Loaded OwnerCommands");
+    }
+
+    // Bot Owner Only Commands go here.
+}
