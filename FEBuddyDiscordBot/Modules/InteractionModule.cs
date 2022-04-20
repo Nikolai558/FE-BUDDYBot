3decead435de7c366e69f1eaa45a97287666079c
Nikolia558 <38259407+Nikolai558@users.noreply.github.com>
2022-04-20 10:10:59 -0600

    Added Give role Slash Command

--- FEBuddyDiscordBot/Modules/InteractionModule.cs
+++ FEBuddyDiscordBot/Modules/InteractionModule.cs
@@ -1,4 +1,7 @@
 ﻿using Discord.Interactions;
+using FEBuddyDiscordBot.DataAccess.DB;
+using FEBuddyDiscordBot.Models;
+using FEBuddyDiscordBot.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +16,7 @@ public class InteractionModule: InteractionModuleBase<SocketInteractionContext>
     private readonly IConfiguration _config;
     private readonly DiscordSocketClient _discord;
     private readonly ILogger _logger;
+    private readonly IMongoGuildData _guildData;
 
     public InteractionModule(IServiceProvider services)
     {
@@ -20,15 +24,22 @@ public class InteractionModule: InteractionModuleBase<SocketInteractionContext>
         _config = _services.GetRequiredService<IConfiguration>();
         _discord = _services.GetRequiredService<DiscordSocketClient>();
         _logger = _services.GetRequiredService<ILogger<InteractionModule>>();
+        _guildData = _services.GetRequiredService<IMongoGuildData>();
 
         _logger.LogDebug("Module: Loaded InteractionModule");
     }
 
-
-    [Discord.Interactions.RequireOwner]
-    [SlashCommand("ping", "Receive a ping message! ")]
-    public async Task PingCommand()
+    [SlashCommand("give-role", "Get discord roles/permissions. Your Discord account must be linked on the VATUSA website.")]
+    public async Task AssignRoles()
     {
-        await RespondAsync("PING!");
+        GuildModel guild = await _guildData.GetGuildAsync(Context.Guild.Id);
+        await _services.GetRequiredService<RoleAssignmentService>().GiveRole((SocketGuildUser)Context.User, guild);
+        await RespondAsync("I gave you roles! Maybe... I dont really know, this seems to be my response everytime regardless of status.");
     }
+
+    //[SlashCommand("ping", "Receive a ping message! ")]
+    //public async Task PingCommand()
+    //{
+    //    await RespondAsync("PING!");
+    //}
 }
