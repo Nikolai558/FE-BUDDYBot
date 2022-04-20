1fe6bba14dcc25f2a874f27d83cb1dcecf97ac81
Nikolia558 <38259407+Nikolai558@users.noreply.github.com>
2022-04-20 14:28:10 -0600

    Moved InteractionModule into Folder

--- FEBuddyDiscordBot/Modules/SlashCommands/InteractionModule.cs
+++ FEBuddyDiscordBot/Modules/SlashCommands/InteractionModule.cs
@@ -8,7 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FEBuddyDiscordBot.Modules;
+namespace FEBuddyDiscordBot.Modules.SlashCommands;
 
 public class InteractionModule: InteractionModuleBase<SocketInteractionContext>
 {
