20aa4103becb19a74bfe06d9caa24ed4c1fdbd76
Nikolia558 <38259407+Nikolai558@users.noreply.github.com>
2022-04-20 21:15:40 -0600

    Added comments for CommandHandler.cs

--- FEBuddyDiscordBot/Services/CommandHandler.cs
+++ FEBuddyDiscordBot/Services/CommandHandler.cs
@@ -2,8 +2,13 @@
 using FEBuddyDiscordBot.Models;
 
 namespace FEBuddyDiscordBot.Services;
+
+/// <summary>
+/// Handle Discord Prefix Commands
+/// </summary>
 public class CommandHandler
 {
+    // Dependency Injection Services Required
     private readonly IServiceProvider _services;
     private readonly IConfiguration _config;
     private readonly DiscordSocketClient _discord;
@@ -11,6 +16,10 @@ public class CommandHandler
     private readonly ILogger _logger;
     private readonly IMongoGuildData _guildData;
 
+    /// <summary>
+    /// Constructor for Command Handler
+    /// </summary>
+    /// <param name="services">Dependency Injection Service Provider</param>
     public CommandHandler(IServiceProvider services)
     {
         _services = services;
@@ -25,6 +34,11 @@ public class CommandHandler
         _logger.LogDebug("Loaded: CommandHandler");
     }
 
+    /// <summary>
+    /// Handle Discord Messages that have the correct prefix and execute the command received.
+    /// </summary>
+    /// <param name="arg">Socket Message from discord.</param>
+    /// <returns>None</returns>
     private async Task HandleCommand(SocketMessage arg)
     {
         GuildModel? guild = null;
@@ -32,36 +46,42 @@ public class CommandHandler
 
         if (message == null) return;
 
-        if (message.Source != MessageSource.User) return;
+        if (message.Source != MessageSource.User) return; // Only respond to user messages
 
         SocketGuildChannel? channel = message.Channel as SocketGuildChannel;
         
-        if (channel != null)
+        if (channel != null) // Make sure we are in a discord server channel (not DM to bot)
         {
-            guild = await _guildData.GetGuildAsync(channel.Guild.Id);
+            guild = await _guildData.GetGuildAsync(channel.Guild.Id); // Load configuration from Database
         }
 
-        int argPosition = 0;
+        int argPosition = 0; // Prefix position in message
 
-        var context = new CommandContext(_discord, message);
+        var context = new CommandContext(_discord, message); // Create a new context for the message
 
         char prefix = Char.Parse(guild?.Settings.Prefix ?? _config.GetRequiredSection("DefaultBotSettings").GetRequiredSection("Prefix").Value);
 
-        if(!message.HasCharPrefix(prefix, ref argPosition)) return;
+        if(!message.HasCharPrefix(prefix, ref argPosition)) return; // Message does not have the guild command prefix so do nothing
 
-        var result = await _commands.ExecuteAsync(context, argPosition, _services);
+        var result = await _commands.ExecuteAsync(context, argPosition, _services); // Execute the command
 
-        await LogCommandUse(context, result);
+        await LogCommandUse(context, result); // Log the command usage
 
         if (!result.IsSuccess)
         {
             if (result.ErrorReason != "Unknown command.")
             {
-                await message.Author.SendMessageAsync($"**Error:** {result.ErrorReason}");
+                await message.Author.SendMessageAsync($"**Error:** {result.ErrorReason}"); // Tell the user the command faild.
             }
         }
     }
 
+    /// <summary>
+    /// Log what user did what command in what server
+    /// </summary>
+    /// <param name="context">Context for the message the command is in</param>
+    /// <param name="result">IResult for the result of the command</param>
+    /// <returns>None</returns>
     private async Task LogCommandUse(CommandContext context, IResult result)
     {
         await Task.Run(() =>
