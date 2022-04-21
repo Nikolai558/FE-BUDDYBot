becc3d6111b2d6b537b91498a150346764f14475
Nikolia558 <38259407+Nikolai558@users.noreply.github.com>
2022-04-21 12:35:23 -0600

    Added AdminSlashCommand Module

---
+++ FEBuddyDiscordBot/Modules/SlashCommands/AdminSlashCommands.cs
@@ -0,0 +1,32 @@
+﻿using Discord.Interactions;
+using FEBuddyDiscordBot.DataAccess.DB;
+
+namespace FEBuddyDiscordBot.Modules.SlashCommands;
+
+/// <summary>
+/// Admin Discord Slash Commands and Interactions
+/// </summary>
+public class AdminSlashCommands: InteractionModuleBase<SocketInteractionContext>
+{
+    // Dependency Injection Services Required
+    private readonly IServiceProvider _services;
+    private readonly IConfiguration _config;
+    private readonly DiscordSocketClient _discord;
+    private readonly ILogger _logger;
+    private readonly IMongoGuildData _guildData;
+
+    /// <summary>
+    /// Initialize the Admin Slash Commands Module (This might be unnecessary)
+    /// </summary>
+    /// <param name="services">Dependency Injection Service Provider</param>
+    public AdminSlashCommands(IServiceProvider services)
+    {
+        _services = services;
+        _config = _services.GetRequiredService<IConfiguration>();
+        _discord = _services.GetRequiredService<DiscordSocketClient>();
+        _logger = _services.GetRequiredService<ILogger<AdminSlashCommands>>();
+        _guildData = _services.GetRequiredService<IMongoGuildData>();
+
+        _logger.LogDebug("Module: Loaded AdminSlashCommands");
+    }
+}
