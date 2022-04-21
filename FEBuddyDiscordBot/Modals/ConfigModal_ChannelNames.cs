3a0d3707e6b1577616cd870d22a085a81e67eba3
Nikolia558 <38259407+Nikolai558@users.noreply.github.com>
2022-04-21 16:52:36 -0600

    Added Discord Modals

---
+++ FEBuddyDiscordBot/Modals/ConfigModal_ChannelNames.cs
@@ -0,0 +1,16 @@
+﻿using Discord.Interactions;
+
+namespace FEBuddyDiscordBot.Modals;
+
+public class ConfigModal_ChannelNames : IModal
+{
+    public string Title => "Discord Channel Name Server Configuration";
+
+    [InputLabel("Private Meeting Voice Channel Name")]
+    [ModalTextInput("PrivateMeetingVoiceChannelName", TextInputStyle.Short, maxLength: 50)]
+    public string PrivateMeetingVoiceChannelName { get; set; }
+
+    [InputLabel("Roles Text Channel Name")]
+    [ModalTextInput("RolesTextChannelName", TextInputStyle.Short, maxLength: 50)]
+    public string RolesTextChannelName { get; set; }
+}
