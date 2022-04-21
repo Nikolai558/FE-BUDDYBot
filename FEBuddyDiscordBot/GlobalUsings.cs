ac28c70b58c6cf6c9d5122e3254e6ef2616bc2e1
Nikolia558 <38259407+Nikolai558@users.noreply.github.com>
2022-04-20 20:31:33 -0600

    Added comment to GlobalUsing.cs

--- FEBuddyDiscordBot/GlobalUsings.cs
+++ FEBuddyDiscordBot/GlobalUsings.cs
@@ -1,4 +1,6 @@
-﻿global using Discord;
+﻿// Global usings make it so that we do not need to specify using statements in all of our files.
+
+global using Discord;
 global using Discord.Commands;
 global using Discord.WebSocket;
 global using Microsoft.Extensions.Configuration;
