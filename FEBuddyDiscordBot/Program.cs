6bba35846ad16639fe1f5e6c607b32c7073b400f
Nikolia558 <38259407+Nikolai558@users.noreply.github.com>
2022-04-20 20:30:37 -0600

    Added Code Comments to Program.cs

--- FEBuddyDiscordBot/Program.cs
+++ FEBuddyDiscordBot/Program.cs
@@ -1,15 +1,24 @@
 ﻿namespace FEBuddyDiscordBot;
 
+/// <summary>
+/// Main entrypoint Class for program start
+/// </summary>
 class Program
 {
-    public static void Main(string[] args)
+    /// <summary>
+    /// The Main method is the entry point of an executable program; it is where the program control starts and ends.
+    /// </summary>
+    public static void Main()
     {
+        // This will catch any exceptions thrown by synchronous code.
         try
         {
+            // Create and start the FeBuddy Discord Bot
             new FeBuddyBot().StartAsync().GetAwaiter().GetResult();
         }
         catch (Exception ex)
         {
+            // Log the exception and crash the program.
             Console.WriteLine(ex.Message);
             throw;
         }
